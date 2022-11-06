c1a66052d0bd9fbc7b75881f317faa2683692068
Brena <brenarodrigues@alu.ufc.br>
2022-11-06 14:44:00 -0300

    l1-2-q2 concluida

--- lista1-2/L1-2-Q2/src/TCPClient.java
+++ lista1-2/L1-2-Q2/src/TCPClient.java
@@ -1,4 +1,5 @@
 import java.net.*;
+<<<<<<< HEAD
 import java.util.Scanner;
 import java.io.*;
 
@@ -98,3 +99,25 @@ public class TCPClient {
 		}
 	}
 }
+=======
+import java.io.*;
+public class TCPClient {
+	public static void main (String args[]) {
+	// arguments supply message and hostname of destination
+		Socket s = null;
+	    try{
+	    	int serverPort = 7896;
+		   	s = new Socket(args[1], serverPort);
+		   	DataInputStream in = new DataInputStream( s.getInputStream());
+			DataOutputStream out = new DataOutputStream(s.getOutputStream());
+			out.writeUTF(args[0]);        	// UTF is a string encoding see Sn 4.3
+			String data = in.readUTF();
+			System.out.println("Received: "+ data);
+	    } catch (UnknownHostException e){System.out.println("Sock:"+e.getMessage()); 
+	    } catch (EOFException e){ System.out.println("EOF:"+e.getMessage());
+	    } catch (IOException e){ System.out.println("IO:"+e.getMessage());
+		} finally { if(s!=null) { try { s.close(); } catch (IOException e){System.out.println("close:"+e.getMessage());}}
+		}
+  	}
+}
+>>>>>>> 06511d374696b7c64973b59f5b6f2250399e62a0
